e730de8460cdff8e22289cf8cd32a7f412f8f3c6
AJ Bates <aj.bates2.92@gmail.com>
2021-03-24 01:48:12 -0500

    created landing page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,63 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+				<meta
+					name='description'
+					content='Web app for tip based workers to keep better track of there money'
+				/>
+			</Head>
+			<header className={styles.header_container}>
+				<div>
+					<h1 className={styles.tips_header}>tips.</h1>
+					<p>Keep track of your money.</p>
+					<Link href='/dashboard'>
+						<a>Create an account</a>
+					</Link>
+				</div>
+			</header>
+			<nav className={styles.nav}>
+				<ul className={styles.nav_list}>
+					<li>
+						<Link href='/dashboard'>
+							<a>login</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/dashboard'>
+							<a>sign up</a>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<main>
+				<div className={styles.about}>
+					<h2 className={utilStyles.headingLg}>
+						The service industry isn't a 'fake' job.
+					</h2>
+					<p>
+						The money definitely isn't. Keep track of your income
+						like a 'real' adult. Making informed financial decisions
+						is infinitely easier when you actually know your
+						finances.
+					</p>
+				</div>
+				<div className={styles.demo_creds}>
+					<div>
+						<h3>Demo Credentials</h3>
+						<p>
+							email: foo@bar.com <br />
+							password: password
+						</p>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
